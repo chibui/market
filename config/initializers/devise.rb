f2cd89b3ca9c5e4760d53aa97bd4a96dffce2b64
chi <chi_bui@outlook.com>
2016-10-21 15:25:48 +1100

    fix env var

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,7 +249,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.secret_key = '948480c76940e1256be64b7e6315e66d2b9a2650f985235d23cb0c97b8b3c94d6a9301778457107d0b654a0cd8689031bced296dcac8cf9801f66ca56ee959df'
-  config.omniauth :facebook, Rails.application.secrets.APP_ID, Rails.application.secrets.APP_SECRET, scope: 'email', info_fields: 'email',
+  config.omniauth :facebook,  ENV['APP_ID'], ENV['APP_SECRET'], scope: 'email', info_fields: 'email',
   callback_url: "http://localhost:3000/users/auth/facebook/callback",
   :client_options => {:ssl => {:ca_path => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 
