2008958096a327cce545d233b6df1fc820d2e746
chi <chi_bui@outlook.com>
2016-10-21 21:51:21 +1100

    remove seed items

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,5 @@ ActiveRecord::Base.connection.disable_referential_integrity do
   u3 = User.create({name: "Kev's Shop", email: "kev@example.com", password: "123456", password_confirmation: "123456", role_id: r2.id, contact: "Kev", phone: 43536356, address: "235 sdflskdf", abn: "1234", bio: "bio", image: "", tos_status: true })
   u4 = User.create({name: "chi", email: "chi@example.com", password: "123456", password_confirmation: "123456", role_id: r3.id, contact: "Market", phone: 43536356, address: "235 sdflskdf", abn: "1234", bio: "bio", image: "", tos_status: true })
 
-  i1 = Item.create({name: "Apple", description: "Delicious granny smith apple", origin: "sydney", min_order: 1, price: 3.99, item_image: "", user_id: u2.id})
-  i2 = Item.create({name: "Banana", description: "Sweet lady finger banana", origin: "melbourne", min_order: 1, price: 2.99, item_image: "", user_id: u2.id})
-  i3 = Item.create({name: "Orange", description: "Navel Orange", origin: "gold coast", min_order: 1, price: 9.99, item_image: "", user_id: u3.id})
-  i4 = Item.create({name: "Pineapple", description: "Super Pineapple", origin: "perth", min_order: 1, price: 19.99, item_image: "", user_id: u3.id})
 
 end
